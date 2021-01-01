f32c183adff2df5992f491dd3498a0cce119c6ca
Matt Chang <matthewhansol@gmail.com>
2021-01-01 16:06:08 -0500

    feat: added carousel to portfolio

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -3,6 +3,9 @@ import Head from 'next/head'
 import Layout from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedProjectsData } from '../lib/projects'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
 
 export async function getStaticProps() {
   const allProjectsData = await getSortedProjectsData()
@@ -22,7 +25,16 @@ export default function Portfolio({ allProjectsData }) {
                 </Head>
                 <h1>Portfolio</h1>
             </div>
-            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+            <div className="container">
+            <Slider>
+                {
+                    allProjectsData.map(project => (
+                        <img src={project.image} />
+                    ))
+                }
+            </Slider>
+            </div>
+            {/* <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
                 <ul className={utilStyles.list}>
                 {allProjectsData.map(({ id, date, title, role, image, mobileImage, contentHtml }) => (
                     <li className={utilStyles.listItem} key={id}>
@@ -40,7 +52,7 @@ export default function Portfolio({ allProjectsData }) {
                     </li>
                 ))}
                 </ul>
-            </section>
+            </section> */}
         </Layout>
     )
 }
