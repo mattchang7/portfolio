29058eb6086a6916b367bf71a186cfe8bef25263
Matt Chang <matthewhansol@gmail.com>
2021-01-06 16:56:37 -0500

    feat: added animations to nav bar and home page

--- components/layout.js
+++ components/layout.js
@@ -1,25 +1,58 @@
+/* eslint-disable react/react-in-jsx-scope */
 import styles from '../styles/layout.module.css'
 import Link from 'next/link'
+import { motion } from 'framer-motion'
 
-export default function Layout({ children, home }) {
+export default function Layout({ children }) {
   return (
         <div className={styles.container}>
-            <div className={styles.navBar}>
+            <motion.div className={styles.navBar} initial={{ opacity: 0 }} animate={{ opacity: 1}} transition={{ duration: 2 }} >
                 <Link href='/'>
-                     <a className={styles.logo}><img src='images/logo-svg.svg'/></a>
+                    <motion.a 
+                        className={styles.logo}
+                        initial={{ x: '-8vw' }}
+                        animate={{ x: 0 }}
+                        transition={{ duration: 1, delay: 0.5, type: 'spring', stiffness: 120 }}
+                    >
+                        <img src='images/logo-svg.svg'/>
+                    </motion.a>
                 </Link>
                 <div className={styles.navLinksBox}>
                     <Link href='/portfolio'>
-                        <a className={styles.navLinks} title='Portfolio'>Portfolio</a>
+                        <motion.a 
+                            className={styles.navLinks} 
+                            title='Portfolio'
+                            initial={{ x: '20vw' }}
+                            animate={{ x: 0 }}
+                            transition={{ duration: 1, type: 'spring', stiffness: 70 }}
+                        >
+                            Portfolio
+                        </motion.a>
                     </Link>
                     <Link href='/experience'>
-                        <a className={styles.navLinks} title='Experience'>Experience</a>
+                        <motion.a 
+                            className={styles.navLinks} 
+                            title='Experience'
+                            initial={{ x: '20vw' }}
+                            animate={{ x: 0 }}
+                            transition={{ duration: 1, delay: 0.25, type: 'spring', stiffness: 70 }}
+                        >
+                            Experience
+                        </motion.a>
                     </Link>
                     <Link href='/contact'>
-                        <a className={styles.navLinks} title='Contact'>Contact</a>
+                        <motion.a 
+                            className={styles.navLinks} 
+                            title='Contact'
+                            initial={{ x: '20vw' }}
+                            animate={{ x: 0 }}
+                            transition={{ duration: 1, delay: 0.5, type: 'spring', stiffness: 70 }}
+                        >
+                            Contact
+                        </motion.a>
                     </Link>
                 </div>
-            </div>
+            </motion.div>
             <div className={styles.body}>
                 {children}
             </div>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Layout from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { motion } from 'framer-motion'
 
 export default function Home() {
   return (
@@ -9,10 +10,32 @@ export default function Home() {
         <title>Matt Chang</title>
         <link rel='icon' href='/images/logo-icon.ico'/>
       </Head>
-      <section>
-        <img src='images/profile-bw.jpg' className={utilStyles.profile} />
-        <p>Hi, I'm a web developer</p>
-      </section>
+      <motion.section 
+        // initial={{ x: '10vw', y: '60vw', opacity: 0 }}
+        // animate={{ color: '#aa98b6', fontSize: '5vw', opacity: 1, y: 0 }}
+        // transition={{ duration: 1.5 }}
+      >
+        <motion.img 
+          src='images/profile-bw.jpg' 
+          className={utilStyles.profile} 
+          animate={{ y: '5vw' }} 
+          transition={{ delay: 0.5 }}
+        />
+        <motion.h1 
+          initial={{ x: '10vw', y: '20vw', fontSize: '6vw', opacity: 0 }}
+          animate={{ y: 0, opacity: 1, fontSize: '6vw', color: '#aa98b6' }}
+          transition={{ duration: 3.5, type: 'spring', stiffness: 80, delay: 0.5  }}
+        >
+          Hi, I'm a
+        </motion.h1>
+        <motion.h1 
+          initial={{ x: '10vw', y: '30vw', fontSize: '6vw', opacity: 0 }}
+          animate={{ y: '-7vw', opacity: 1, fontSize: '6vw', color: '#aa98b6' }} 
+          transition={{ duration: 3.5, delay: 0.6, type: 'spring', stiffness: 80 }}
+        >
+          web developer
+        </motion.h1>
+      </motion.section>
     </Layout>
   )
 }
