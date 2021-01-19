28c4b180c29e9f70a2b34dc02acbb624b0d28b4a
Matt Chang <matthewhansol@gmail.com>
2021-01-19 13:47:36 -0500

    feat: added animations to experience timeline

--- pages/experience.js
+++ pages/experience.js
@@ -4,6 +4,7 @@ import Head from 'next/head'
 import Layout from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedExperiencesData } from '../lib/experience'
+import { motion } from 'framer-motion'
 
 export async function getStaticProps() {
   const allExperiencesData = await getSortedExperiencesData()
@@ -21,22 +22,31 @@ export default function Experience({ allExperiencesData }) {
                 <title>Experience</title>
             </Head>
             <div className={utilStyles.experienceContainer}>
-                <div className={utilStyles.timeLine}>
+                <motion.div
+                    className={utilStyles.timeLine}
+                    initial={{ y: '100vh', opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 1.5, type: 'spring', stiffness: 50 }}
+                >
                     <ul>
                         {
-                            allExperiencesData.map(({ name, image, startDate, endDate, role, contentHtml }) => (
-                                <li>
-                                    <div key={name} className={utilStyles.timeLineContent} >
+                            allExperiencesData.map(({ name, image, startDate, endDate, role, contentHtml }, index) => (
+                                <motion.li
+                                    initial={{ opacity: 0 }}
+                                    animate={{ opacity: 1 }}
+                                    transition={{ duration: 1, delay: (index / 2) + 0.5 }}
+                                >
+                                    <div key={name} className={utilStyles.timeLineContent}>
                                         <h6 className={utilStyles.timeLineDate}>{startDate} to {endDate}</h6>
                                         <img className={utilStyles.experienceLogo} src={image}/>
                                         <h5>{role}</h5>
                                         <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
                                     </div>
-                                </li>
+                                </motion.li>
                             ))
                         }
                     </ul>
-                </div>
+                </motion.div>
             </div>
         </Layout>
     )
