2550acf02398a2f0de635484e7a7c192654e89d3
Matt Chang <matthewhansol@gmail.com>
2021-01-27 12:52:07 -0500

    fix: icon size 2

--- pages/contact.js
+++ pages/contact.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/react-in-jsx-scope */
 import Head from 'next/head'
 import Layout from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
@@ -14,10 +15,26 @@ export default function Contact() {
                     <link rel='icon' href='/public/images/logo-icon.ico'/>
                 </Head>
                 <div className={utilStyles.contactContainer}>
-                    <motion.h4 initial={{ y: '30vh', opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={transition} ><i className="fas fa-thumbtack fa-sm"/>    New York, NY</motion.h4>
-                    <motion.h4 initial={{ y: '30vh', opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={transition} ><i className="fas fa-envelope fa-sm"/>    matthewhansol@gmail.com</motion.h4>
-                    <motion.h4 initial={{ y: '30vh', opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={transition} ><i className="fab fa-linkedin fa-sm"/><a href='https://www.linkedin.com/in/mattchang7'>    https://www.linkedin.com/in/mattchang7</a></motion.h4>
-                    <motion.h4 initial={{ y: '30vh', opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={transition} ><i className="fab fa-github fa-sm"/><a href='https://github.com/mattchang7'>    https://github.com/mattchang7</a></motion.h4>
+                    <motion.h4 
+                        initial={{ y: '30vh', opacity: 0 }} 
+                        animate={{ y: 0, opacity: 1 }} 
+                        transition={{ duration: 0.6, type: 'spring', stiffness: 50, delay: 0.3}}
+                    ><i className="fas fa-thumbtack fa-sm" width={0} />    New York, NY</motion.h4>
+                    <motion.h4 
+                        initial={{ y: '30vh', opacity: 0 }} 
+                        animate={{ y: 0, opacity: 1 }} 
+                        transition={{ duration: 0.6, type: 'spring', stiffness: 50, delay: 0.5}}
+                    ><i className="fas fa-envelope fa-sm" width={0} />    matthewhansol@gmail.com</motion.h4>
+                    <motion.h4 
+                        initial={{ y: '30vh', opacity: 0 }} 
+                        animate={{ y: 0, opacity: 1 }} 
+                        transition={{ duration: 0.6, type: 'spring', stiffness: 50, delay: 0.7}}
+                    ><i className="fab fa-linkedin fa-sm" width={0} /><a href='https://www.linkedin.com/in/mattchang7'>    https://www.linkedin.com/in/mattchang7</a></motion.h4>
+                    <motion.h4
+                        initial={{ y: '30vh', opacity: 0 }}
+                        animate={{ y: 0, opacity: 1 }}
+                        transition={{ duration: 0.6, type: 'spring', stiffness: 50, delay: 0.9}}
+                    ><i className="fab fa-github fa-sm" width={0} /><a href='https://github.com/mattchang7'>    https://github.com/mattchang7</a></motion.h4>
                 </div>
             </div>
         </Layout>
